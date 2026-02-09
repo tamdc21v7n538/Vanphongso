27d59b91e2950c501344e1ef22d25bb041bf9827
Tran Huu Tam <tamdc21v7n538@vlvh.ctu.edu.vn>
2026-02-09 13:46:43 +0700

    init spring boot backend - run successfully

---
+++ src/main/java/com/vanphongso/tam/VanphongsoApplication.java
@@ -0,0 +1,13 @@
+package com.vanphongso.tam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanphongsoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VanphongsoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vanphongso/tam/VanphongsoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanphongso.tam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VanphongsoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
